bbbad2377957b046522f3dae1f39e83ee2c9c946
Klinger Oliveira <klingeroliveira@hotmail.com>
2020-03-18 16:22:20 -0300

    .

--- 2020/launchbase/module_03/server.js
+++ 2020/launchbase/module_03/server.js
@@ -1,19 +1,25 @@
+
+// 1 - instalar express - framework web para criação do servidor
 const express = require('express') //carrega o pacote na variavel
+
+//4- instalar nunjuks (npm install nunjuks)
 const nunjucks = require('nunjucks') //motor de templates, views, reuso de código
 
+// 2- carregar em uma variável as funções do express
 const server = express() //carrega a função para a variavel servidor
 
-//criando pasta public para colocar arquivos estaticos
+//8- definindo a pasta para arquivos estaticos (scripts, css...)
 server.use(express.static('public'))
 
-//configurando tamplate engine
+//5- configurando o tipo de arquivo a ser renderizado e enviado ao servidor
 server.set("view engine", "html")
 
+//6- indicando pasta padrão para o server, com nunjucks
 nunjucks.configure("views", {
     express: server
 })
 
-//função para uma roda, o servidor receber a requisição (request) e responde (response)
+//7- rotas com função get para receber a requisição (request) e responder (response) a chamada da página
 server.get("/", function(req, res){
     return res.render("about")
 })
@@ -22,7 +28,8 @@ server.get("/portifolio", function(req, res){
     return res.render("portifolio")
 })
 
-//iniciando o servidor na porta 5000
+//3 - definindo a porta para execução doservidor 
+//e instalar nodemon como dependências de desenvolvimento (npm install -D nodemon)
 server.listen(5000, function(){
     console.log('server is running')
 })
---
+++ 2020/launchbase/module_03_01/public/scripts.js
@@ -0,0 +1,25 @@
+const modalOverlay = document.querySelector(".modal-overlay");
+const modal = document.querySelector(".modal");
+const cards = document.querySelectorAll(".card");
+
+for (let card of cards) {
+    card.addEventListener("click" , function() {
+        const videoId = card.getAttribute("id");
+        modalOverlay.classList.add("active");
+        modalOverlay.querySelector("iframe").src = `https://rocketseat.com.br/${videoId}`;
+    })
+}
+
+document.querySelector(".close-modal").addEventListener("click", function(){
+    modalOverlay.classList.remove("active");
+    modalOverlay.querySelector("iframe").src = "";
+    if (modal.classList.contains("maximize")){
+        modal.classList.remove("maximize");
+    }
+})
+
+document.querySelector(".maximize-modal").addEventListener("click", function(){
+    if (modal.classList.contains("maximize")){
+        modal.classList.remove("maximize");
+    } else modal.classList.add("maximize");
+})
