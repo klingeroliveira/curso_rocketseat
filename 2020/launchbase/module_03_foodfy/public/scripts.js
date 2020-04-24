24b761901d1e099b5f37050fe447464064a6c212
klingeroliveira <60447413+klingeroliveira@users.noreply.github.com>
2020-04-23 22:05:01 -0300

    .

--- 2020/launchbase/module_03_foodfy/public/scripts.js
+++ 2020/launchbase/module_03_foodfy/public/scripts.js
@@ -1,34 +1,30 @@
-//const modalOverlay = document.querySelector('.modal_overlay');
 const cards = document.querySelectorAll('.card');
-
+const expandDetails = document.querySelectorAll('a.expandDetails');
 
 
 for (let card of cards) {
-    card.addEventListener("click", function(){
+    card.addEventListener("click", function () {
         const idCard = card.getAttribute("id");
-        //const recipeIndex = req.params.index;
-        console.log(idCard);
         window.location.href = `/recipes/${idCard}`;
     })
 }
-/*
-for (let card of cards){
-    card.addEventListener("click", function(){
-        const modalImg = card.getAttribute("id");
-        const pCardContent = card.querySelector(".p_card_content");
-        const pCardInfo = card.querySelector(".p_card_info");
 
-        modalOverlay.classList.add('active');
-        modalOverlay.querySelector("img").src = `images/${modalImg}`;
-        document.getElementById('p_modal_card_content').innerHTML = pCardContent.innerHTML;
-        document.getElementById('p_modal_card_info').innerHTML = pCardInfo.innerHTML;
-
+for (let expand of expandDetails) {
+    expand.addEventListener("click", function () {
+        if (expand.getAttribute('name').toString = "preparation") {
+            const preparation = document.querySelector('.recipe_info_preparation');
+            console.log(expand.textContent);
+            hidden(expand,preparation);
+        }
     })
 }
 
-document.querySelector('.modal_close').addEventListener("click", function(){
-    modalOverlay.classList.remove("active");
-    modalOverlay.querySelector("img").src = "";
-    document.getElementById('p_modal_card_content').innerHTML = "";
-    document.getElementById('p_modal_card_info').innerHTML = "";
-})*/
+function hidden(expand, preparation){
+    if (expand.textContent = "ESCONDER"){
+        preparation.setAttribute('hidden', true);
+        expand.textContent = "MOSTRAR";
+     }else {
+         preparation.removeAttribute('hidden');
+         expand.textContent = "ESCONDER";
+     }
+}
