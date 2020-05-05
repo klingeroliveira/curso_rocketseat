cfef866da23c1c2cf4ce4df36d5aeeb5d92cbea4
klingeroliveira <60447413+klingeroliveira@users.noreply.github.com>
2020-05-04 21:11:43 -0300

    .

--- 2020/launchbase/module_04/routes.js
+++ 2020/launchbase/module_04/routes.js
@@ -9,6 +9,22 @@ routes.get('/instructors', function(req,res){
     return res.render("instructors/index")
 })
 
+routes.get('/instructors/create', function(req,res){
+    return res.render("instructors/create")
+})
+
+routes.post('/instructors', function(req,res){
+
+    const keys = Object.keys(req.body)
+
+    for (key of keys){
+        if (req.body[key] == "")
+            return res.send("Preencha todos os campos.")
+    }
+
+    return res.send(keys)
+})
+
 routes.get('/members', function(req,res){
     return res.send("members")
 })
