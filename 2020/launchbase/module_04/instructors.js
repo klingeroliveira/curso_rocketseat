3ea3d54f1470200074561fbdfbfc1c820799c422
klingeroliveira <60447413+klingeroliveira@users.noreply.github.com>
2020-05-06 20:38:09 -0300

    .

--- 2020/launchbase/module_04/instructors.js
+++ 2020/launchbase/module_04/instructors.js
@@ -1,6 +1,6 @@
 
 const fs = require('fs')
-
+const data = require('./data.json')
 //create
 exports.post = function(req,res){
 
@@ -11,7 +11,16 @@ exports.post = function(req,res){
             return res.send("Preencha todos os campos.")
     }
 
-    fs.writeFile("data.json", JSON.stringify(req.body), function(err){
+    let { avartar_url, name, birth, gender, services} = req.body
+
+    const id = Number(data.instructors.length + 1)
+    const created_at = Date.now()
+
+    birth = Date.parse(birth)
+    
+    data.instructors.push( {id, avartar_url, name, birth, gender, services, created_at} )
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
         if (err) return res.send("Falha ao gravar o arquivo!")
 
         return res.redirect('/instructors')
