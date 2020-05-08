fa961558e83b9d6167f0f21d0789be239f4e9764
Klinger Oliveira <klingeroliveira@hotmail.com>
2020-05-08 17:13:23 -0300

    .

---
+++ 2020/launchbase/module_04_03/teachers.js
@@ -0,0 +1,16 @@
+
+
+//POST create
+exports.post =  function (req, res){
+
+    const keys = Object.keys(req.body)
+
+    for (key of keys){
+        if (req.body[key] == ""){
+            return res.send("Preencha todos os campos!")
+        }
+    }
+
+    
+    return res.render("teachers/create")
+}
