beeb30246c72ddc944a7c6f53bddda63eb133b53
Klinger Oliveira <klingeroliveira@hotmail.com>
2020-06-12 18:12:05 -0300

    .

--- 2020/launchbase/module_05/src/lib/utils.js
+++ 2020/launchbase/module_05/src/lib/utils.js
@@ -6,11 +6,13 @@ module.exports = {
         let age = today.getFullYear() - birthDate.getFullYear()
         const month = today.getMonth() - birthDate.getMonth()
 
-        if (month < 0 ||
-            month == 0 && today.getDate() <= birthDate.getDate()) {
+        if (month < 0 || (month == 0 && today.getDate() <= birthDate.getDate())) {
             age = age - 1
         }
-
+        console.log(month)
+        console.log(today.getDate()) 
+        console.log(birthDate.getDate())
+        console.log(age)
         return age
 
     },
